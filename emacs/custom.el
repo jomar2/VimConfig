13caf1fc432a20b82270c4db323f846a5fe79aba
jomar2 <joel.martinsson@gmail.com>
2017-05-27 16:16:56 +0200

    Starting to get there, based on AB-Configs.

---
+++ emacs/custom.el
@@ -0,0 +1,36 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default bold shadow italic underline bold bold-italic bold])
+ '(ansi-color-names-vector
+   ["#3c3836" "#fb4934" "#b8bb26" "#fabd2f" "#83a598" "#d3869b" "#8ec07c" "#ebdbb2"])
+ '(fci-rule-color "#d6d6d6")
+ '(markdown-preview-custom-template "/Users/abieber/.emacs.d/markdown-preview-template.html")
+ '(org-agenda-files nil)
+ '(org-hide-emphasis-markers t)
+ '(org-modules
+   (quote
+    (org-bbdb org-bibtex org-docview org-gnus org-habit org-info org-irc org-mhe org-rmail org-w3m)))
+ '(package-selected-packages
+   (quote
+    (neotree smooth-scroll s tiny-menu helm-make all-the-icons-dired all-the-icons rjsx-mode json-mode lua-mode helm-spotify-plus go-projectile jinja2-mode cyberpunk-theme go-mode org-evil jujube-theme smart-mode-line-powerline-theme smart-mode-line visual-fill-column websocket flycheck powerline evil key-chord color-theme-modern esup counsel-projectile restclient ox-reveal org-tree-slide epresent color-moccur xterm-color nlinum-relative company-shell pandoc-mode virtualenvwrapper counsel helm-swoop groovy-mode octopress zenburn-theme yaml-mode which-key wgrep-ag web-mode w3m use-package twittering-mode sunshine sublime-themes rainbow-mode powerline-evil org-bullets mmm-mode markdown-mode magit highlight-symbol helm-projectile gtags fullframe flycheck-package exec-path-from-shell evil-surround evil-leader evil-jumper evil-indent-textobject emmet-mode elpy dictionary color-theme-sanityinc-tomorrow bpr auto-complete ag)))
+ '(safe-local-variable-values (quote ((css-indent-offset . 2) (no-byte-compile t)))))
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:family "Consolas" :foundry "nil" :slant normal :weight normal :height 140 :width expanded))))
+ '(all-the-icons-dired-dir-face ((t (:foreground "khaki2"))))
+ '(powerline-evil-normal-face ((t (:inherit powerline-evil-base-face :background "chartreuse3"))))
+ '(sml/folder ((t (:inherit sml/global :background "grey22" :foreground "grey50" :weight normal))))
+ '(sml/git ((t (:background "grey22" :foreground "chartreuse3"))))
+ '(term ((t (:foreground "ivory1"))))
+ '(term-color-black ((t (:foreground "gray80"))))
+ '(term-color-cyan ((t (:foreground "cyan2"))))
+ '(term-color-green ((t (:foreground "OliveDrab3"))))
+ '(term-color-yellow ((t (:foreground "gold1")))))
