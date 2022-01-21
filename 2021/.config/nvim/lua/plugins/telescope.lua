515b7eac871c68f42e95eca4f81f912f8e108f60
Joel Martinsson <Joel.Martinsson@vaderstad.com>
2022-01-21 18:31:35 +0100

    Some updates..

--- 2021/.config/nvim/lua/plugins/telescope.lua
+++ 2021/.config/nvim/lua/plugins/telescope.lua
@@ -3,36 +3,17 @@ if not present then
     return
 end
 
-local os = vim.loop.os_uname().sysname
-if os == "Linux" then
-  telescope.setup {
-    extensions = {
-      media_files = {
-        filetypes = { "png", "webp", "jpg", "jpeg" },
-        find_cmd = "rg"
-      },
-      fzf = {
-        fuzzy = true,
-        override_generic_sorter = true,
-        override_file_sorter = true,
-        case_mode = "smart_case",
-      },
-    },
+telescope.setup {
+  extensions = {
+    fzf = {
+      fuzzy = true,
+      override_generic_sorter = true,
+      override_file_sorter = true,
+      case_mode = "smart_case",
+    }
+  },
+  defaults = {
+    path_display = {"truncate"},
   }
---  telescope.load_extension("media_files")
---  telescope.load_extension("find_directories")
-  telescope.load_extension("fzf")
-else
-  telescope.setup {
-    extensions = {
-      fzf = {
-        fuzzy = true,
-        override_generic_sorter = true,
-        override_file_sorter = true,
-        case_mode = "smart_case",
-      }
-    },
-  }
-  telescope.load_extension("fzf")
-  telescope.load_extension("find_directories")
-end
+}
+telescope.load_extension('fzf')
--- 2021/.config/nvim/lua/settings.lua
+++ 2021/.config/nvim/lua/settings.lua
@@ -6,20 +6,14 @@ opt.number = true
 opt.numberwidth = 2
 opt.relativenumber = true
 
--- Set scroll offset.
-opt.scrolloff = 8
-
--- Remove showing mode.
-opt.showmode = false
-
 -- True collor support.
 opt.termguicolors = true
 
 -- Enable clipboard.
-opt.clipboard = "unnamedplus"
+opt.clipboard = 'unnamedplus'
 
 -- Enable mouse in all modes.
-opt.mouse = "a"
+opt.mouse = 'a'
 
 -- Enable cursor line.
 opt.cursorline = true
@@ -27,22 +21,22 @@ opt.cursorline = true
 -- Setting colorcolumn. This is set because of
 -- this (https://github.com/lukas-reineke/indent-blankline.nvim/issues/59)
 -- indent-blankline bug.
-opt.colorcolumn = "9999"
+opt.colorcolumn = '9999'
 
 -- With set hidden you’re telling Neovim that you can
 -- have unsaved worked that’s not displayed on your screen.
 opt.hidden = true
 
 -- Set indentation stuf.
-opt.tabstop = 4
-opt.shiftwidth = 4
+opt.tabstop = 2
+opt.shiftwidth = 2
 opt.smartindent = true
 opt.smartcase = true
 opt.expandtab = true
 opt.smarttab = true
 
 -- Set searching stuf.
-opt.hlsearch = false
+opt.hlsearch = true
 opt.incsearch = true
 opt.ignorecase = true
 
@@ -51,7 +45,7 @@ opt.ignorecase = true
 opt.termbidi = true
 
 -- Without this option some times backspace did not work correctly.
-opt.backspace = "indent,eol,start"
+opt.backspace = 'indent,eol,start'
 
 -- For opening splits on right or bottom.
 opt.splitbelow = true
@@ -59,6 +53,7 @@ opt.splitright = true
 
 -- Enabling ruler and statusline.
 opt.ruler = true
+opt.laststatus = 2
 
 -- Setting time that Neovim wait after each keystroke.
 opt.ttimeoutlen = 20
@@ -67,36 +62,26 @@ opt.timeoutlen = 1000
 -- Setting up autocomplete menu.
 opt.completeopt = "menuone,noselect"
 
-opt.swapfile = false
-opt.undodir="~/.vim/undodir/"
-opt.undofile = true
-opt.backup = false
--- Add cursorline and diasable it in terminal
---vim.cmd("autocmd WinEnter,BufEnter * if &ft is \"toggleterm\" | set nocursorline | else | set cursorline | endif")
-
 -- Set line number for help files.
 vim.cmd
 [[
-augroup help_config
-  autocmd!
-  autocmd FileType help :set number
-augroup END
+  augroup help_config
+    autocmd!
+    autocmd FileType help :set number
+  augroup END
 ]]
 
 -- Auto open nvim-tree when writing (nvim .) in command line
 -- and auto open Dashboard when nothing given as argument.
+--[[
 vim.cmd
 [[
-if index(argv(), ".") >= 0
-  autocmd VimEnter * NvimTreeToggle
-  bd1
-elseif len(argv()) == 0
-  autocmd VimEnter * Dashboard
-endif
-]]
+  if index(argv(), ".") >= 0
+    autocmd VimEnter * NvimTreeToggle
+    bd1
+  elseif len(argv()) == 0
+    autocmd VimEnter * Dashboard
+  endif
+]]--
+vim.cmd('autocmd BufWritePost plugins.lua source <afile>')
 
-
-vim.cmd("autocmd BufWritePost plugins.lua source <afile>")
-
-
---vim.cmd("command CodeArtTransparent lua make_codeart_transparent()")
